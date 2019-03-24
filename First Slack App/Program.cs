1b06ca913ddbe3f31efc7b0eda4cea6679975626
Kristen Kinnear-Ohlmann <kristenkinnearohlmann@gmail.com>
2019-03-24 07:49:57 -0500

    First commit

---
+++ First Slack App/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace First_Slack_App
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string token = "xoxp-532079122384-532814801122-546875286194-3a8402049f6206ad01760a53a3bdb069";
+            var writer = new SlackMessageWriter(token);
+            Task.WaitAll(writer.WriteMessage("Hello from C#!"));
+            Task.WaitAll(writer.WriteSecretMessage("14144246"));
+        }
+    }
+}
---
+++ First Slack App/SlackMessageWriter.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+
+namespace First_Slack_App
+{
+    class SlackMessageWriter
+    {
+        private readonly string _url;
+        private readonly HttpClient _client;
+
+        public SlackMessageWriter(string token)
+        {
+            _url = "https://slack.com/api/chat.postMessage";
+            _client = new HttpClient();
+            _client.DefaultRequestHeaders.Authorization = new System.Net.Http.Headers.AuthenticationHeaderValue("Bearer", token);
+        }
+
+        public async Task WriteMessage(string message)
+        {
+            var postObject = new { channel = "#general", text = message };
+            var json = JsonConvert.SerializeObject(postObject);
+            var content = new StringContent(json, Encoding.UTF8, "application/json");
+
+            await _client.PostAsync(_url, content);
+        }
+
+        public async Task WriteSecretMessage(string secretMessage)
+        {
+            uint hash = 5381;
+            uint i = 0;
+
+            for (i=0; i < secretMessage.Length; i++)
+            {
+                hash = ((hash << 5) + hash) + ((byte)secretMessage[(int)i]);
+            }
+
+            await WriteMessage(System.Convert.ToString(hash));
+        }
+    }
+}
